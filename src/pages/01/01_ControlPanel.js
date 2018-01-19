f4932170e4cb27d5dbcdb15cc8a8608b84bff766
bnguyensn <b.nguyen.sn@gmail.com>
2018-01-19 00:30:49 +0000

    Change message input bar width to using a hard-coded 600px

--- src/pages/01/01_ControlPanel.js
+++ src/pages/01/01_ControlPanel.js
@@ -37,12 +37,8 @@ function TextInput(props) {
         props.handleChange(e);
     }
 
-    function toggleTextBarExpansion(e) {
-        props.toggleTextBarExpansion(e);
-    }
-
     return (
-        <div className={`cp-inp-container ${props.widthControl}`} name='message' onTransitionEnd={toggleTextBarExpansion}>
+        <div className={`cp-inp-container ${props.widthControl}`} name='message'>
             <input className={`cp-inp ${props.isShown}`}
                    name={props.name}
                    type='text'
@@ -63,12 +59,9 @@ class ControlPanel extends Component {
         this.actionTweet = this.actionTweet.bind(this);
         this.actionHelp = this.actionHelp.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.toggleTextBarExpansion = this.toggleTextBarExpansion.bind(this);
 
         this.state = {
             message_btn_clicked: false,
-            message_bar_expanded: false,
-            message_field_isShown: 'hidden',
             message: ''
         }
     }
@@ -83,7 +76,6 @@ class ControlPanel extends Component {
         this.setState((prevState, props) => {
             return {
                 message_btn_clicked: !prevState.message_btn_clicked,
-                message_field_isShown: prevState.message_bar_expanded ? 'hidden' : prevState.message_field_isShown
             }
         });
     }
@@ -104,16 +96,6 @@ class ControlPanel extends Component {
         });
     }
 
-    toggleTextBarExpansion(e) {
-        const name = e.target.getAttribute('name');
-        this.setState((prevState, props) => {
-            return {
-                [`${name}_bar_expanded`]: !prevState[`${name}_bar_expanded`],
-                [`${name}_field_isShown`]: prevState[`${name}_bar_expanded`] ? prevState[`${name}_field_isShown`] : ''
-            }
-        });
-    }
-
     /* ********** MAIN ********** */
 
     render() {
@@ -121,7 +103,7 @@ class ControlPanel extends Component {
             'min-width-100p': this.state.message_btn_clicked
         });
         const inp_msg_width_control = classnames({
-            'width-100p': this.state.message_btn_clicked,
+            'cp-inp-container-max': this.state.message_btn_clicked,
         });
 
         return (
@@ -129,12 +111,10 @@ class ControlPanel extends Component {
                 <div className={`cp-bar ${bar_width_control}`}>
                     <Button name='btn-msg' icon='message' handleClick={this.msgBtnClicked} />
                     <TextInput widthControl={inp_msg_width_control}
-                               isShown={this.state.message_field_isShown}
                                name='message'
                                placeholder='Type in some message...'
                                value={this.state.message}
-                               handleChange={this.handleInputChange}
-                               toggleTextBarExpansion={this.toggleTextBarExpansion} />
+                               handleChange={this.handleInputChange} />
                     <ButtonSVG path='M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z'
                                name='btn-twitter'
                                handleClick={this.actionTweet} />
