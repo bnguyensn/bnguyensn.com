6956d977624f50bdb5c7699f89b20c38ad401d9d
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-05-14 23:26:20 +0100

    Add initial <WorldTravelMap/> files

---
+++ src/widgets/world-travel-map/WorldTravelMap.js
@@ -0,0 +1,22 @@
+// @flow
+'use strict';
+
+import React, {PureComponent} from 'react';
+
+import worldMap from './img/world2.svg';
+
+import './world-travel-map.css';
+
+class WorldTravelMap extends PureComponent<{}> {
+    render() {
+        return (
+            <div id='world-travel-map'>
+                <svg role="img" viewBox="0 0 2000 1001" xmlns="http://www.w3.org/2000/svg">
+
+                </svg>
+            </div>
+        )
+    }
+}
+
+export default WorldTravelMap
