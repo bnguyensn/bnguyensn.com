38157230814175e567ee668fa5dc1fc6d641668b
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-05-20 12:17:05 +0100

    Add .svg world map and React world map generator

--- src/widgets/world-travel-map/WorldTravelMap.js
+++ src/widgets/world-travel-map/WorldTravelMap.js
@@ -2,17 +2,29 @@
 'use strict';
 
 import React, {PureComponent} from 'react';
+import type {Node} from 'react';
 
-import worldMap from './img/world2.svg';
+import worldMapData from './img/world2';
 
 import './world-travel-map.css';
 
 class WorldTravelMap extends PureComponent<{}> {
+    countryIds: string[];
+    countryPathElements: Node;
+
+    constructor() {
+        super();
+        this.countryIds = Object.keys(worldMapData);
+        this.countryPathElements = this.countryIds.map((countryId) =>
+            <path key={countryId} d={worldMapData[countryId].d} fill={'#81C784'} />
+        );
+    }
+
     render() {
         return (
             <div id='world-travel-map'>
                 <svg role="img" viewBox="0 0 2000 1001" xmlns="http://www.w3.org/2000/svg">
-
+                    {this.countryPathElements}
                 </svg>
             </div>
         )
