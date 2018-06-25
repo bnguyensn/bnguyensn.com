1c272cedc5db1b7b345f2949c74b0c3ddf8c4642
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-06-25 23:58:47 +0100

    Add preliminary <Footer />

---
+++ src/homepage/components/Footer.js
@@ -0,0 +1,15 @@
+// @flow
+
+import * as React from 'react';
+
+import '../css/footer.css';
+
+function Footer() {
+    return (
+        <section>
+            Placeholder
+        </section>
+    )
+}
+
+export default Footer
