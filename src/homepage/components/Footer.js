d04e85f797d8400b8afe3fd8053de7ce2fa47f48
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-07-09 23:22:36 +0100

    Update <Footer />

--- src/homepage/components/Footer.js
+++ src/homepage/components/Footer.js
@@ -6,8 +6,10 @@ import '../css/footer.css';
 
 function Footer() {
     return (
-        <section>
-            Placeholder
+        <section id="index-footer">
+            <span>
+                Site created by <a href="https://twitter.com/bnguyensn">@bnguyensn</a> - Check out the source code <a href="https://github.com/bnguyensn/bnguyensn.com">here</a>.
+            </span>
         </section>
     )
 }
