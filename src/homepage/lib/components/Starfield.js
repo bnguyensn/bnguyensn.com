d3beb4ba1ecfa92b644869565f4e7bc14390688b
bnguyensn <b.nguyen.sn@gmail.com>
2018-07-17 18:44:16 +0100

    Update  animation

--- src/homepage/lib/components/Starfield.js
+++ src/homepage/lib/components/Starfield.js
@@ -10,6 +10,8 @@ import * as React from 'react';
 
 import getRandNumBtw from '../utils/getRandNumBtw';
 
+/** ********** STAR ********** **/
+
 type StarType = {
     context: CanvasRenderingContext2D,
     posX: number,
@@ -33,9 +35,23 @@ function drawStar(starInfo: StarType) {
     if (colour) { context.fillStyle = ogContextColour; }
 }
 
-function createStarLayer(width: number, height: number,
-                         starShadowBlur: number, starShadowColour: string, starColour: string,
-                         starCount: number, starRadiusMin: number, starRadiusMax: number): HTMLCanvasElement {
+type StarLayerType = {
+    width: number,
+    height: number,
+    starShadowBlur: number,
+    starShadowColour: string,
+    starColour: string,
+    starCount: number,
+    starRadiusMin: number,
+    starRadiusMax: number,
+};
+
+/** ********** STAR LAYER ********** **/
+
+function createStarLayer(starLayerInfo: StarLayerType): HTMLCanvasElement {
+    const {width, height, starShadowBlur, starShadowColour, starColour,
+        starCount, starRadiusMin, starRadiusMax} = starLayerInfo;
+
     // Initialise the star layer
     const starCanvas = document.createElement('canvas');
     starCanvas.width = width;
@@ -61,15 +77,65 @@ function createStarLayer(width: number, height: number,
     return starCanvas
 }
 
-function createStarField() {
-    const starLayer1 = createStarLayer(2000, 2000, 2, '#BDBDBD', '#9E9E9E', 100, 3, 5);
+/** ********** ANIMATION ********** **/
 
+function animStep(mainCanvas: HTMLCanvasElement, mainCtx: CanvasRenderingContext2D,
+                  starFields: HTMLImageElement[],
+                  curOffset: number, offsetStep: number) {
+    const newOffset = curOffset + offsetStep;
+
+    // Clear and redraw the canvas with position-updated starfield images
+    mainCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+    for (let i = 0; i < starFields.length; i++) {
+        mainCtx.drawImage(starFields[0], newOffset, 0);
+        mainCtx.drawImage(starFields[0], newOffset - mainCanvas.width, 0);
+    }
+
+    // Call next frame
+    window.requestAnimationFrame(() => {
+        animStep(mainCanvas, mainCtx, starFields, newOffset, offsetStep);
+    });
 }
 
-export default function Starfield() {
-    return (
-        <div>
-            A
-        </div>
-    )
+/** ********** REACT COMPONENT ********** **/
+
+export default class Starfield extends React.PureComponent<{}> {
+    starLayers: HTMLImageElement[];  // Contains an array of image-converted starfields
+    mainCanvas: HTMLCanvasElement;
+
+    constructor(props: {}) {
+        super(props);
+
+        // Create a few star layers and convert them (canvas elements) to images
+        const starLayer1 = createStarLayer({
+            width: 2000,
+            height: 2000,
+            starShadowBlur: 2,
+            starShadowColour:'#BDBDBD',
+            starColour: '#9E9E9E',
+            starCount: 100,
+            starRadiusMin: 3,
+            starRadiusMax: 5,
+        });
+        this.starLayers[0] = document.createElement('img');
+        this.starLayers[0].src = starLayer1.toDataURL();
+
+        // Create the main canvas
+        this.mainCanvas = document.createElement('canvas');
+    }
+
+    componentDidMount = () => {
+            window.requestAnimationFrame(() => {
+                animStep(this.mainCanvas, this.mainCanvas.getContext('2d'), this.starLayers, 0, 5);
+            });
+    };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
 }
