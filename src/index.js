6652b9fe1720ca92ad15ff72c687090e71a134c5
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-04-06 10:31:25 +0100

    Update HMR code in src

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 import './styles/global.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+};
+
+render(App);
 
 // webpack's Hot Module Replacement
 // https://webpack.js.org/guides/hot-module-replacement/
 if (module.hot) {
-  module.hot.accept('./App.js', () => {
-    const Next = require('./App.js');
-    ReactDOM.render(<Next />, document.getElementById('root'));
+  module.hot.accept('./App', () => {
+    render(App);
+    render(require('./App'));
   });
 }
