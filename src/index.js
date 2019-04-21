898d6733721730a0fc555f13379e39f01a3cfa60
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-04-21 13:17:02 +0100

    Update index.js with Service Worker registration

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Register Service Worker
+// https://webpack.js.org/guides/progressive-web-application#adding-workbox
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log(`SW registered: ${registration}`);
+      })
+      .catch(registrationError => {
+        console.log(`SW registration failed: ${registrationError}`);
+      });
+  });
+}
+
+// React index render
 ReactDOM.render(<App />, document.getElementById('root'));
