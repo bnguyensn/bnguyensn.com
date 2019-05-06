e4d7fdb1195b0570daf0467c5a1825a9148b7cc9
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-05-07 00:38:24 +0100

    Update MongoDB code

--- db/connectDb.js
+++ db/connectDb.js
@@ -4,10 +4,11 @@
  * @param client: the MongoClient instance
  * @param url: the MongoDB URL
  * @param dbName: the MongoDB database's name
- * @return Db: the connected Mongo database if the connection is successful
- * @return Error: an error if the connection fails
+ * @return Promise:
+ *  onFulfilled: return the connected Mongo database
+ *  onRejected: return an error
  * */
-async function connectDb(client, url, dbName) {
+function connectDb(client, url, dbName) {
   return client
     .connect()
     .then(res => res.db(dbName), err => new Error(err));
